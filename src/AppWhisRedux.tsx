638b3db0f8251e92f644588296bd1eb2f7da0d1f
David Radchenko <davidradchenko2@gmail.com>
2022-10-28 10:54:17 -0700

    go to TodolistList

--- src/AppWhisRedux.tsx
+++ src/AppWhisRedux.tsx
@@ -4,19 +4,7 @@ import {AppBar, Button, Container, Grid, IconButton, LinearProgress, Paper, Tool
 import MenuIcon from '@mui/icons-material/Menu';
 import {TaskStatuses, TaskType} from "./api/TodolistAPI";
 import {CustomizedSnackbars} from "./ErrorSnakBar";
-import {useSelector} from "react-redux";
-import {RootState, useAppDispatch} from "./State/store";
-import {
-    addTodolistTC,
-    ChangeIsdoneTodoAC,
-    ChehgeTitleTodolistTC,
-    deleteTodolistTC, fetchTodolistTC,
-    FilterType,
-    TodolistDomainType
-} from "./State/todolists-reducer";
-import {addTaskTC, deleteTasksTC, updateTaskStatusTC} from "./State/tasks-reducer";
-import {AddItemForm} from "./AddItemForm";
-import {Todolists} from "./Todolists";
+import {TodolistList} from "./TodolistList";
 
 
 export  type TodoTasksType={
@@ -24,41 +12,7 @@ export  type TodoTasksType={
 }
 
 function AppWhisRedux() {
-    const status = useSelector<RootState, string | null>(state => state.app.status)
-    const dispatch = useAppDispatch()
 
-    const TodolistData = useSelector<RootState, Array<TodolistDomainType>>(state => state.todolists)
-    const tasksObj = useSelector<RootState, TodoTasksType>(state => state.tasks)
-
-    const removeTask = useCallback(function (id: string, todolistId: string) {
-        dispatch(deleteTasksTC(todolistId, id))
-    }, [dispatch])
-    const AddNewTodoTask = useCallback(function (title: string, todolistId: string) {
-        dispatch(addTaskTC(todolistId, title))
-    }, [dispatch])
-    const ChengeTitleTodo = useCallback(function (idTodo: string, NewTitle: string) {
-        dispatch(ChehgeTitleTodolistTC(idTodo, NewTitle))
-    }, [dispatch])
-    const ChengeTaskTitle = useCallback(function (idTodo: string, idTask: string, title: string) {
-        dispatch(updateTaskStatusTC(idTodo, {title}, idTask))
-    }, [dispatch])
-    const DeleteTodo = useCallback(function (id: string) {
-        dispatch(deleteTodolistTC(id))
-    }, [dispatch])
-    const chengeTaskChecked = useCallback(function (idTask: string, status: TaskStatuses, idTodo: string) {
-        dispatch(updateTaskStatusTC(idTodo, {status}, idTask))
-    }, [dispatch])
-    const chengeTasksFilter = useCallback(function (value: FilterType, todolistId: string) {
-        dispatch(ChangeIsdoneTodoAC(value, todolistId))
-    }, [dispatch])
-    const AddTodolistButtonProps = useCallback((title: string) => {
-        dispatch(addTodolistTC(title))
-
-    }, [dispatch])
-//call thunks
-    useEffect(() => {
-        dispatch(fetchTodolistTC())
-    }, [])
     return (
 
             <div className="App">
@@ -80,37 +34,7 @@ function AppWhisRedux() {
                 { status === 'loading' &&  <LinearProgress color="secondary"/>}
             </AppBar>
             <Container fixed>
-
-                <Grid container style={{padding: "20px 0px 20px 0px"}}>
-                    <AddItemForm AddItem={AddTodolistButtonProps}/>
-                </Grid>
-                <Grid container spacing={10}>
-                    {TodolistData.map((tl) => {
-
-                        let filtrData = tasksObj[tl.id]
-                        let taskfortodolist = filtrData
-
-                        return (
-                            <Grid key={tl.id} item>
-                                <Paper style={{padding: "10px"}}>
-                                    <Todolists
-                                        todolist={tl}
-                                        key={tl.id}
-                                        ChengeTitleTodo={ChengeTitleTodo}
-                                        ChengeTaskName={ChengeTaskTitle}
-                                        tasks={taskfortodolist}
-                                        removeTask={removeTask}
-                                        FilterChenge={chengeTasksFilter}
-                                        chengeChecked={chengeTaskChecked}
-                                        DeleteTodo={DeleteTodo}
-                                        AddNewTodoTask={AddNewTodoTask}/>
-                                </Paper>
-                            </Grid>
-
-                        )
-                    })}
-                </Grid>
-
+                <TodolistList/>
             </Container>
             <CustomizedSnackbars/>
         </div>
