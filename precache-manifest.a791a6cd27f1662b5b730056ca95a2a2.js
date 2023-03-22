61b3511a7cb87c212fc401c656267c7a11a8fa52
David Radchenko <davidradchenko2@gmail.com>
2023-03-22 10:42:18 -0700

    add files

---
+++ precache-manifest.a791a6cd27f1662b5b730056ca95a2a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67bd99f81c0543415b4a",
+    "url": "/static/css/main.528b4a15.chunk.css"
+  },
+  {
+    "revision": "67bd99f81c0543415b4a",
+    "url": "/static/js/main.67bd99f8.chunk.js"
+  },
+  {
+    "revision": "40266387d098bb4a1e17",
+    "url": "/static/js/1.40266387.chunk.js"
+  },
+  {
+    "revision": "5b28936ea3d821724a27",
+    "url": "/static/js/2.5b28936e.chunk.js"
+  },
+  {
+    "revision": "809caacfed65c9277ebd",
+    "url": "/static/js/runtime~main.809caacf.js"
+  },
+  {
+    "revision": "a5972ca4a88fb0e507d34bc575639b34",
+    "url": "/index.html"
+  }
+];
